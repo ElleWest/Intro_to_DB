6f68d4c409f6cf6edf96efe2c204b9c7a01858fc
GitHub User <user@example.com>
2025-10-19 20:10:21 +0200

    Simplify task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,2 @@
--- Prints the full description of the table books from the database alx_book_store
--- Without using DESCRIBE or EXPLAIN statements
-
 USE alx_book_store;
-SHOW COLUMNS FROM books;
+SHOW COLUMNS FROM Books;
