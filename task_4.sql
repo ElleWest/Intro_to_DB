0e62c085206ab829fd81ef67fb4e2972b0fa5d23
GitHub User <user@example.com>
2025-10-19 20:08:08 +0200

    Fix task_4.sql table description

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
--- Show full description of books table without using DESCRIBE or EXPLAIN
--- This script displays the structure of the books table using SHOW COLUMNS
+-- Prints the full description of the table books from the database alx_book_store
+-- Without using DESCRIBE or EXPLAIN statements
 
 USE alx_book_store;
-SHOW COLUMNS FROM Books;
+SHOW COLUMNS FROM books;
